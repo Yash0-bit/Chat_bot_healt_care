3ce448fa322902276d830efbaf9fb89a8ce7ab17
Yash0-bit <solankeyash090@gmail.com>
2023-10-08 11:36:00 +0530

    modified app.py

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
 from flask import Flask, render_template, request, jsonify
-
-app = Flask("Chatbot_K_LLC")
-
-# Add your Healthcare ChatBot model and functions here
-import pandas as pd
 from fuzzywuzzy import process
+import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 import numpy as np
+from sklearn import preprocessing
+
+app = Flask(__name__)
 
 # Load the training data
-training = pd.read_csv('C:/Users/Yash/Downloads/healthcare-chatbot-master/Data/Training.csv')
+training = pd.read_csv('C:/Users/Yash/OneDrive/Desktop/Study/Chat_bot_kratin_LLC/Data/Training.csv')
 cols = training.columns[:-1]
 x = training[cols]
 y = training['prognosis']
 
 # Label encode the target variable
-from sklearn import preprocessing
 le = preprocessing.LabelEncoder()
 le.fit(y)
 y = le.transform(y)
@@ -25,8 +23,8 @@ clf = DecisionTreeClassifier()
 clf.fit(x, y)
 
 # Load symptom data and descriptions
-symptom_description = pd.read_csv('C:/Users/Yash/Downloads/healthcare-chatbot-master/MasterData/symptom_Description.csv', index_col=0)
-symptom_precautions = pd.read_csv('C:/Users/Yash/Downloads/healthcare-chatbot-master/MasterData/symptom_precaution.csv', index_col=0)
+symptom_description = pd.read_csv('C:/Users/Yash/OneDrive/Desktop/Study/Chat_bot_kratin_LLC/MasterData/symptom_Description.csv', index_col=0)
+symptom_precautions = pd.read_csv('C:/Users/Yash/OneDrive/Desktop/Study/Chat_bot_kratin_LLC/MasterData/symptom_precaution.csv', index_col=0)
 
 def fuzzy_match_symptom(input_symptom):
     # Use fuzzy matching to find the closest matching symptom
@@ -41,27 +39,28 @@ def chatbot_response(user_message):
     matched_symptom = fuzzy_match_symptom(user_message)
 
     if matched_symptom:
-        num_days = 1  # Default value, you can modify this
-
         input_vector = np.zeros(len(cols))
         input_vector[cols.get_loc(matched_symptom)] = 1
         prediction = clf.predict([input_vector])
 
         present_disease = le.inverse_transform(prediction)[0]
-
-        symptoms_given = list(symptom_precautions.columns[symptom_precautions.loc[present_disease].notna()])
-        symptoms_exp = [symptom for symptom in symptoms_given if user_message.lower() == 'yes']
+        response = f"You may have {present_disease}\n{symptom_description.loc[present_disease]}"
 
         second_prediction = le.inverse_transform(clf.predict([input_vector]))[0]
         if present_disease != second_prediction:
-            return f"You may also have {second_prediction}\n{symptom_description.loc[second_prediction]}"
-        else:
-            return f"You may have {present_disease}\n{symptom_description.loc[present_disease]}"
+            response += f"\nYou may also have {second_prediction}\n{symptom_description.loc[second_prediction]}"
+
+        precautions = list(symptom_precautions.loc[present_disease].dropna())
+        response += "\nTake the following precautions:"
+        for i, precaution in enumerate(precautions):
+            response += f"\n{i + 1}) {precaution}"
+
+        return response
     else:
         return "Enter a valid symptom."
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/chat', methods=['POST'])
